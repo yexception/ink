f6dca93451b5bfcece2fecdc7336171c10ea2caa
yAsceDark <srgamezao2004@gmail.com>
2026-08-06 20:32:20 -0300

    rename

---
+++ src/config/DatabaseConnection.java
@@ -0,0 +1,14 @@
+package config;
+
+import java.sql.Connection;
+
+/*public class DatabaseConnection {
+    private static final String URL = "";
+    private static final String USER = "...";
+    private static final String PASSWORD = "...";
+
+    public static Connection getConnection(){
+
+    }
+}
+*/
